3894a57e17de9f4cc5cb4bec050e0942822d8951
21delenclo <matt.delenclos@gmail.com>
2022-01-28 15:29:29 +0100

    mvnt

--- cpp/Keyboard.cpp
+++ cpp/Keyboard.cpp
@@ -3,32 +3,32 @@
 #include<vector>
 #include"Map.h"
 
-void movement(Map &playground, int x, int y){
+void movement(Map &playground, int x, int y, Personnage perso){
 
     int indice = (playground).get(x,y);
     std::string touche;
     std::cin >> touche ;
     if(touche == "z"){
-        if( (((playground.get_content())[indice]) == '.') || (((playground.get_content())[indice])== '#') || (((playground.get_content())[indice]) == '+')){
-            (playground).set(x,y+1,'x');
+        if( (((playground.get_content())[playground.get(x+1,y)]) == '.') || (((playground.get_content())[playground.get(x+1,y)])== '#') || (((playground.get_content())[playground.get(x+1,y)]) == '+')){
+            (playground).set(x+1,y,'x');
             (playground).set(x,y,'.');}
-        //Ici on met les autres if : si y'a des ennemis à attaquer, des pieces a recup etc
+        if()
 
     };
     if(touche == "q"){
-        if( (((playground.get_content())[indice]) == '.') || (((playground.get_content())[indice])== '#') || (((playground.get_content())[indice]) == '+')){
-            (playground).set(x-1,y,'x');
+        if( (((playground.get_content())[indice-1]) == '.') || (((playground.get_content())[indice-1])== '#') || (((playground.get_content())[indice-1]) == '+')){
+            (playground).set(x,y-1,'x');
             (playground).set(x,y,'.');}
     };
     if(touche == "s"){
-        if( (((playground.get_content())[indice]) == '.') || (((playground.get_content())[indice])== '#') || (((playground.get_content())[indice]) == '+')){
-            (playground).set(x,y-1,'x');
+        if( (((playground.get_content())[playground.get(x-1,y)]) == '.') || (((playground.get_content())[playground.get(x-1,y)])== '#') || (((playground.get_content())[playground.get(x-1,y)]) == '+')){
+            (playground).set(x-1,y,'x');
             (playground).set(x,y,'.');}
         
     };
     if(touche == "d"){
-        if( (((playground.get_content())[indice]) == '.') || (((playground.get_content())[indice])== '#') || (((playground.get_content())[indice]) == '+')){
-            (playground).set(x+1,y,'x');
+        if( (((playground.get_content())[indice+1]) == '.') || (((playground.get_content())[indice+1])== '#') || (((playground.get_content())[indice+1]) == '+')){
+            (playground).set(x,y+1,'x');
             (playground).set(x,y,'.');}
         
     };
