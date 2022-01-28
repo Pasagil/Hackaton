097cba8260956a4d25b17f247313357b83cfde96
Camille DUBOIS <camdubois02@gmail.com>
2022-01-28 15:05:44 +0100

    représentation de pièce

--- cpp/objet.cpp
+++
@@ -1,7 +0,0 @@
-Objet::ramasser(objet){
-
-}
-
-Objet::placer_dans_sac(objet){
-    
-}
--- includes/init.h
+++ includes/init.h
@@ -4,14 +4,9 @@
 #include "Personnage.h"
 #include "Map.h"
 
-Map init_map(std::pair<int, int> size, int nrooms = 2)
+Map init_map(std::pair<int, int> size, std::vector<Room> rooms)
 {
     Map map(size);
-    std::vector<Room> rooms;
-    rooms.push_back(Room(std::make_pair(3, 5), std::make_pair(1,1), std::make_pair(1,2)));
-    rooms.push_back(Room(std::make_pair(6, 4), std::make_pair(9,12), std::make_pair(9+6,12+4)));
     map.add_room(rooms[0]);
-    map.add_room(rooms[1]);
-
     return map;
 }
